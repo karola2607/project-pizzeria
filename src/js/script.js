add0d22bfbc74b0d7f0a09ecd47e292b17fa4705
Karolina Pastor <borek.karola@gmail.com>
2021-01-24 15:42:44 +0100

    Add function for shopping cart

--- src/js/script.js
+++ src/js/script.js
@@ -328,7 +328,7 @@ const templates = {
       thisCart.products = [];
 
       thisCart.getElements(element);
-
+      thisCart.initActions();
       console.log('new Cart', thisCart);
     }
 
@@ -338,8 +338,20 @@ const templates = {
       thisCart.dom = {};
 
       thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
     }
+
+     initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      })
+     };
   }
+
+
 
 
   const app = {
